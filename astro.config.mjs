671506cccce27f34c98f649edf6699dd602f8cee
ymcheung <ymcheung.tw@me.com>
2022-08-27 16:05:29 +0800

    Use Deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-import solid from "@astrojs/solid-js";
+import deno from '@astrojs/deno';
+import solid from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: deno(),
   integrations: [solid()]
 });
