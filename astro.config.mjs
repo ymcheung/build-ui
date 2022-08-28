27906c92bda1aa966da81d5e0254f92f8189d8b3
ymcheung <ymcheung.tw@me.com>
2022-08-28 12:14:31 +0800

    Add Post layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import deno from '@astrojs/deno';
 import solid from '@astrojs/solid-js';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: deno(),
-  integrations: [solid()]
+  integrations: [solid(), sitemap(), mdx()],
+  site: 'https://build.intersection.tw',
+  trailingSlash: 'never'
 });
