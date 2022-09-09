51feb61913aafc309ade95cef7e66ef9328346fd
ymcheung <ymcheung.tw@me.com>
2022-09-10 00:19:43 +0800

    Update article styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,17 @@ export default defineConfig({
   adapter: deno(),
   integrations: [solid(), sitemap(), mdx()],
   markdown: {
-    syntaxHighlight: 'prism'
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'dracula',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
   },
   site: 'https://build.intersection.tw',
-  trailingSlash: 'never'
+  trailingSlash: 'never',
 });
