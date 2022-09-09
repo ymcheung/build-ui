2b58dc460f70012f40426926f350580a595fa5a6
ymcheung <ymcheung.tw@me.com>
2022-09-10 00:44:53 +0800

    Use dynamic components for Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,11 +13,7 @@ export default defineConfig({
     syntaxHighlight: 'shiki',
     shikiConfig: {
       theme: 'dracula',
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
       langs: [],
-      // Enable word wrap to prevent horizontal scrolling
       wrap: true,
     },
   },
