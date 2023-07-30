529c9b64b3fbe51ec32e2a45f97fb3f8b53b778c
ymcheung <ymcheung.tw@me.com>
2023-07-30 11:50:50 +0800

    Update figure

--- src/markdoc/config/tags/index.ts
+++ src/markdoc/config/tags/index.ts
@@ -1,3 +1,4 @@
+export { default as figure } from "./figure.markdoc";
 export { default as foreword } from "./foreword.markdoc";
 export { default as quote } from "./quote.markdoc";
 export { default as editorNote } from "./editorNote.markdoc";
