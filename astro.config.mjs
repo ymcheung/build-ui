62b5147ae9a7146ed89adfd9abe789d3caf959bb
ymcheung <ymcheung.tw@me.com>
2024-05-31 13:11:55 +0800

    Upgrade Astro to 4.9.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,19 +13,6 @@ export default defineConfig({
     sitemap({
       filter: (page) =>
         page !== 'https://build.intersection.tw/naming-conventions'
-      // customPages: [
-      //   'https://build.intersection.tw/zed-editor',
-      //   'https://build.intersection.tw/chakra-ui-with-react-hook-form',
-      //   'https://build.intersection.tw/mdx-typescript',
-      //   'https://build.intersection.tw/analytics-tool-splitbee',
-      //   'https://build.intersection.tw/css-in-js-stitches',
-      //   'https://build.intersection.tw/using-astro',
-      //   'https://build.intersection.tw/react-i18next',
-      //   'https://build.intersection.tw/remix-with-mdx-bundler',
-      //   'https://build.intersection.tw/how-to-favicon',
-      //   'https://build.intersection.tw/translations',
-      //   'https://build.intersection.tw/ui-translations'
-      // ]
     }),
     markdoc()
   ],
