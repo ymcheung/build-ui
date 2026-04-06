36c9d1dc3a13b1fee3cfbbf7e0970c0e38402db2
ymcheung <ymcheung.tw@me.com>
2026-04-06 17:08:58 +0800

    Add InterAnalytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_INTER_ANALYTICS_SITE_KEY?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
